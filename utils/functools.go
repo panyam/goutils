0cfc1fde80ea6b895ce4c07088be93537267ce68
Sri Panyam <sri.panyam@gmail.com>
2023-04-25 12:08:22 -0700

    Adding a mostly generic batch get method

--- utils/functools.go
+++ utils/functools.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"sync"
+)
+
 func Map[T any, U any](items []T, mapfunc func(T) U) (out []U) {
 	for _, item := range items {
 		out = append(out, mapfunc(item))
@@ -13,5 +17,25 @@ func Filter[T any](items []T, filtfunc func(T) bool) (out []T) {
 			out = append(out, item)
 		}
 	}
+	return
+}
+
+func BatchGet[T any](ids []string, reqMaker func(string) (T, error)) (out map[string]T) {
+	var wg sync.WaitGroup
+	var respMutex sync.Mutex
+	out = make(map[string]T)
+	for _, id := range ids {
+		wg.Add(1)
+		go func(id string) {
+			defer wg.Done()
+			resp, err := reqMaker(id)
+			if err == nil {
+				respMutex.Lock()
+				out[id] = resp
+				respMutex.Unlock()
+			}
+		}(id)
+	}
+	wg.Wait()
 	return
 }
