ac007b744a630308f8beeb506857e7dd6dd3d15f
Sri Panyam <sri.panyam@gmail.com>
2023-05-03 00:11:47 -0700

    Also adding grpc utils

---
+++ grpc/marshall.go
@@ -0,0 +1,25 @@
+package grpc
+
+import (
+	"errors"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+var marshalOptions = protojson.MarshalOptions{
+	Indent:          "  ",
+	EmitUnpopulated: true,
+	UseProtoNames:   true,
+}
+
+func ProtoFormat(msg protoreflect.ProtoMessage) string {
+	return marshalOptions.Format(msg)
+}
+
+func ProtoToJson(msg protoreflect.ProtoMessage) []byte {
+	if msg == nil {
+		panic(errors.New("Cnanot marshall nil"))
+	}
+	jsonData, _ := marshalOptions.Marshal(msg)
+	return jsonData
+}
