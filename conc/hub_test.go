8a7bbcda790233563e7a6d1d7e496f11685b4296
Sri Panyam <sri.panyam@gmail.com>
2023-05-03 14:05:23 -0700

    Fixing tests with new hub routers

--- conc/hub_test.go
+++ conc/hub_test.go
@@ -5,20 +5,22 @@ import (
 	"github.com/stretchr/testify/assert"
 	"log"
 	"sort"
-	/*
-		"sync"
-	*/
 	"testing"
 )
 
-func TestHub(t *testing.T) {
-	log.Println("===================== TestHub =====================")
-	hub := NewHub[int](nil)
+type Msg struct {
+	topic string
+	value int
+}
+
+func TestHubWithBroadcaster(t *testing.T) {
+	log.Println("===================== TestHubWithBroadcaster =====================")
+	hub := NewHub[Msg](nil)
 
 	var results []string
-	makewriter := func(name string) HubWriter[int] {
-		return func(key string, msg int, err error) error {
-			msgstr := fmt.Sprintf("%03d - %s", msg, name)
+	makewriter := func(name string) HubWriter[Msg] {
+		return func(msg Msg, err error) error {
+			msgstr := fmt.Sprintf("%03d - %s - %s", msg.value, msg.topic, name)
 			results = append(results, msgstr)
 			log.Printf("Received: %s", msgstr)
 			return nil
@@ -31,68 +33,77 @@ func TestHub(t *testing.T) {
 	c3 := hub.Connect(makewriter("c3"))
 	c3.Subscribe("e", "f", "g", "h")
 
-	callback := make(chan int)
+	callback := make(chan Msg)
 	defer close(callback)
-	hub.Send("a", 1, nil, nil)
-	hub.Send("b", 2, nil, nil)
-	hub.Send("c", 3, nil, nil)
-	hub.Send("d", 4, nil, nil)
-	hub.Send("e", 5, nil, nil)
-	hub.Send("f", 6, nil, nil)
-	hub.Send("g", 7, nil, nil)
-	hub.Send("h", 8, nil, callback)
+	hub.Send(Msg{"a", 1}, nil, nil)
+	hub.Send(Msg{"b", 2}, nil, nil)
+	hub.Send(Msg{"c", 3}, nil, nil)
+	hub.Send(Msg{"d", 4}, nil, nil)
+	hub.Send(Msg{"e", 5}, nil, callback)
 	<-callback
 
 	sort.Strings(results)
 	assert.Equal(t, results, []string{
-		"001 - c1",
-		"002 - c1",
-		"003 - c1",
-		"003 - c2",
-		"004 - c1",
-		"004 - c2",
-		"005 - c2",
-		"005 - c3",
-		"006 - c2",
-		"006 - c3",
-		"007 - c3",
-		"008 - c3",
+		"001 - a - c1",
+		"001 - a - c2",
+		"001 - a - c3",
+		"002 - b - c1",
+		"002 - b - c2",
+		"002 - b - c3",
+		"003 - c - c1",
+		"003 - c - c2",
+		"003 - c - c3",
+		"004 - d - c1",
+		"004 - d - c2",
+		"004 - d - c3",
+		"005 - e - c1",
+		"005 - e - c2",
+		"005 - e - c3",
 	})
 	// log.Println("Result after 8 -> h: ", results)
 
 	// Now try to remove subscriptions
 	c1.Unsubscribe("a", "c")
 	c2.Subscribe("a")
-	hub.Send("a", 9, nil, callback)
+	hub.Send(Msg{"a", 6}, nil, callback)
 	<-callback
 	// log.Println("Result after 9 -> a: ", results)
+	sort.Strings(results)
 	assert.Equal(t, results, []string{
-		"001 - c1",
-		"002 - c1",
-		"003 - c1",
-		"003 - c2",
-		"004 - c1",
-		"004 - c2",
-		"005 - c2",
-		"005 - c3",
-		"006 - c2",
-		"006 - c3",
-		"007 - c3",
-		"008 - c3",
-		"009 - c2",
+		"001 - a - c1",
+		"001 - a - c2",
+		"001 - a - c3",
+		"002 - b - c1",
+		"002 - b - c2",
+		"002 - b - c3",
+		"003 - c - c1",
+		"003 - c - c2",
+		"003 - c - c3",
+		"004 - d - c1",
+		"004 - d - c2",
+		"004 - d - c3",
+		"005 - e - c1",
+		"005 - e - c2",
+		"005 - e - c3",
+		"006 - a - c1",
+		"006 - a - c2",
+		"006 - a - c3",
 	})
 
 	hub.Stop()
 }
 
-func TestHubWithBroadcaster(t *testing.T) {
-	log.Println("===================== TestHubWithBroadcaster =====================")
-	hub := NewHub[int](NewBroadcaster[int]())
+func TestHubWithKVRouter(t *testing.T) {
+	log.Println("===================== TestHubWithKVRouter =====================")
+	router := NewKVRouter(func(msg Msg) EventType {
+		return msg.topic
+	})
+	hub := NewHub[Msg](router)
 
 	var results []string
-	makewriter := func(name string) HubWriter[int] {
-		return func(eventKey string, msg int, err error) error {
-			msgstr := fmt.Sprintf("%03d - %s", msg, name)
+	makewriter := func(name string) HubWriter[Msg] {
+		return func(msg Msg, err error) error {
+			msgstr := fmt.Sprintf("%03d - %s - %s", msg.value, msg.topic, name)
 			results = append(results, msgstr)
 			log.Printf("Received: %s", msgstr)
 			return nil
@@ -105,53 +116,56 @@ func TestHubWithBroadcaster(t *testing.T) {
 	c3 := hub.Connect(makewriter("c3"))
 	c3.Subscribe("e", "f", "g", "h")
 
-	callback := make(chan int)
+	callback := make(chan Msg)
 	defer close(callback)
-	hub.Send("a", 1, nil, nil)
-	hub.Send("b", 2, nil, nil)
-	hub.Send("c", 3, nil, nil)
-	hub.Send("d", 4, nil, callback)
+	hub.Send(Msg{"a", 1}, nil, nil)
+	hub.Send(Msg{"b", 2}, nil, nil)
+	hub.Send(Msg{"c", 3}, nil, nil)
+	hub.Send(Msg{"d", 4}, nil, nil)
+	hub.Send(Msg{"e", 5}, nil, nil)
+	hub.Send(Msg{"f", 6}, nil, nil)
+	hub.Send(Msg{"g", 7}, nil, nil)
+	hub.Send(Msg{"h", 8}, nil, callback)
 	<-callback
 
 	sort.Strings(results)
 	assert.Equal(t, results, []string{
-		"001 - c1",
-		"001 - c2",
-		"001 - c3",
-		"002 - c1",
-		"002 - c2",
-		"002 - c3",
-		"003 - c1",
-		"003 - c2",
-		"003 - c3",
-		"004 - c1",
-		"004 - c2",
-		"004 - c3",
+		"001 - a - c1",
+		"002 - b - c1",
+		"003 - c - c1",
+		"003 - c - c2",
+		"004 - d - c1",
+		"004 - d - c2",
+		"005 - e - c2",
+		"005 - e - c3",
+		"006 - f - c2",
+		"006 - f - c3",
+		"007 - g - c3",
+		"008 - h - c3",
 	})
 	// log.Println("Result after 8 -> h: ", results)
 
 	// Now try to remove subscriptions
 	c1.Unsubscribe("a", "c")
 	c2.Subscribe("a")
-	hub.Send("a", 9, nil, callback)
+	hub.Send(Msg{"a", 9}, nil, callback)
 	<-callback
 	// log.Println("Result after 9 -> a: ", results)
+	sort.Strings(results)
 	assert.Equal(t, results, []string{
-		"001 - c1",
-		"001 - c2",
-		"001 - c3",
-		"002 - c1",
-		"002 - c2",
-		"002 - c3",
-		"003 - c1",
-		"003 - c2",
-		"003 - c3",
-		"004 - c1",
-		"004 - c2",
-		"004 - c3",
-		"009 - c1",
-		"009 - c2",
-		"009 - c3",
+		"001 - a - c1",
+		"002 - b - c1",
+		"003 - c - c1",
+		"003 - c - c2",
+		"004 - d - c1",
+		"004 - d - c2",
+		"005 - e - c2",
+		"005 - e - c3",
+		"006 - f - c2",
+		"006 - f - c3",
+		"007 - g - c3",
+		"008 - h - c3",
+		"009 - a - c2",
 	})
 
 	hub.Stop()
