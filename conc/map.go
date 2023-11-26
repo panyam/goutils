4f91f00ec4c45ca12fff8ec15f7fb7f97a91b071
Sri Panyam <sri.panyam@gmail.com>
2023-11-26 11:38:26 -0800

    more simpler apis for Map

--- conc/map.go
+++ conc/map.go
@@ -7,11 +7,36 @@ type Map[K comparable, V any] struct {
 	store   map[K]V
 }
 
-func NewMap[K comparable, V any](out *Map[K, V]) {
+func NewMap[K comparable, V any]() (out *Map[K, V]) {
 	out = &Map[K, V]{
 		store: make(map[K]V),
 	}
 	return
+}
+
+func (m *Map[K, V]) RLock() {
+	m.rwmutex.RLock()
+}
+
+func (m *Map[K, V]) RUnlock() {
+	m.rwmutex.RUnlock()
+}
+
+func (m *Map[K, V]) Lock() {
+	m.rwmutex.Lock()
+}
+
+func (m *Map[K, V]) Unlock() {
+	m.rwmutex.Unlock()
+}
+
+func (m *Map[K, V]) Has(k K, lock bool) bool {
+	if lock {
+		m.rwmutex.RLock()
+		defer m.rwmutex.RUnlock()
+	}
+	_, exists := m.store[k]
+	return exists
 }
 
 func (m *Map[K, V]) Get(k K, lock bool) V {
@@ -52,12 +77,12 @@ func (m *Map[K, V]) Range(lock bool, meth func(K, V) bool) {
 
 func (m *Map[K, V]) View(actions func()) {
 	m.rwmutex.RLock()
-	defer m.rwmutex.RUnlock()
 	actions()
+	m.rwmutex.RUnlock()
 }
 
 func (m *Map[K, V]) Update(actions func()) {
 	m.rwmutex.Lock()
-	defer m.rwmutex.Unlock()
 	actions()
+	m.rwmutex.Unlock()
 }
