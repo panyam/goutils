bdd4bb412656a846c0fd5b5bd63bb9dcdd70a179
Sri Panyam <sri.panyam@gmail.com>
2023-11-26 20:18:55 -0800

    Fixing so that most handler methods move to the conn instead of the handler

--- ws/handlers.go
+++ ws/handlers.go
@@ -11,110 +11,44 @@ import (
 	gut "github.com/panyam/goutils/utils"
 )
 
-type WSHandler[I any, O any, S any] interface {
-	/**
-	 * Called to validate an http request to see if it is upgradeable to a ws conn
-	 */
-	Validate(w http.ResponseWriter, r *http.Request) (S, bool)
-
+type WSConn[I any] interface {
 	/**
 	 * Reads the next message from the ws conn.
 	 */
-	ReadMessage(state S, w *websocket.Conn) (I, error)
+	ReadMessage(w *websocket.Conn) (I, error)
 
 	/**
 	 * Called to send the next ping message.
 	 */
-	SendPing(state S) error
+	SendPing() error
 
 	/**
 	 * Called to handle the next message from the input stream on the ws conn.
 	 */
-	HandleMessage(state S, msg I) error
+	HandleMessage(msg I) error
 
 	/**
 	 * On created.
 	 */
-	OnStart(state S, conn *websocket.Conn) error
+	OnStart(conn *websocket.Conn) error
 
 	/**
 	 * Called to handle or suppress an error
 	 */
-	OnError(state S, err error) error
+	OnError(err error) error
 
 	/**
 	 * Called when the connection closes.
 	 */
-	OnClose(state S)
-	OnTimeout(state S) bool
+	OnClose()
+	OnTimeout() bool
 }
 
-type JSONHandler[C any] struct {
-	Writer *conc.Writer[conc.Message[interface{}]]
-}
-
-func (j *JSONHandler[S]) Validate(w http.ResponseWriter, r *http.Request) (out S, isValid bool) {
-	// All connections upgradeable
-	return
-}
-
-/**
- * Reads the next message from the ws conn.
- */
-func (j *JSONHandler[S]) ReadMessage(state S, conn *websocket.Conn) (out interface{}, err error) {
-	err = conn.ReadJSON(&out)
-	return
-}
-
-/**
- * Called to send the next ping message.
- */
-func (j *JSONHandler[S]) SendPing(state S) error {
-	j.Writer.Send(conc.Message[interface{}]{Value: gut.StringMap{"type": "ping"}})
-	return nil
-}
-
-/**
- * Called to handle the next message from the input stream on the ws conn.
- */
-func (j *JSONHandler[S]) HandleMessage(state S, msg interface{}) error {
-	log.Println("Received Message: ", msg)
-	return nil
-}
-
-func (j *JSONHandler[S]) OnStart(state S, conn *websocket.Conn) error {
-	j.Writer = conc.NewWriter(
-		func(msg conc.Message[interface{}]) error {
-			if msg.Error == io.EOF {
-				log.Println("Streamer closed...", msg.Error)
-				// do nothing
-				// SendJsonResponse(rw, nil, msg.Error)
-				return nil
-			} else if msg.Error != nil {
-				return WSConnWriteError(conn, msg.Error)
-			} else {
-				return WSConnWriteMessage(conn, msg.Value)
-			}
-		})
-	return nil
-}
-
-func (j *JSONHandler[S]) OnError(state S, err error) error {
-	return err
-}
-
-/**
- * Called when the connection closes.
- */
-func (j *JSONHandler[S]) OnClose(state S) {
-	// All the core hapens here
-	if j.Writer != nil {
-		j.Writer.Stop()
-	}
-}
-
-func (j *JSONHandler[S]) OnTimeout(state S) bool {
-	return true
+type WSHandler[I any, S WSConn[I]] interface {
+	/**
+	 * Called to validate an http request to see if it is upgradeable to a ws conn
+	 */
+	Validate(w http.ResponseWriter, r *http.Request) (S, bool)
 }
 
 type WSConnConfig struct {
@@ -135,7 +69,7 @@ func DefaultWSConnConfig() *WSConnConfig {
 	}
 }
 
-func WSServe[I any, O any, S any](h WSHandler[I, O, S], config *WSConnConfig) http.HandlerFunc {
+func WSServe[I any, S WSConn[I]](h WSHandler[I, S], config *WSConnConfig) http.HandlerFunc {
 	if config == nil {
 		config = DefaultWSConnConfig()
 	}
@@ -154,16 +88,17 @@ func WSServe[I any, O any, S any](h WSHandler[I, O, S], config *WSConnConfig) ht
 		}
 		defer conn.Close()
 
-		WSHandleConn[I, O, S](conn, h, ctx, config)
+		log.Println("Start Handling Conn with: ", ctx)
+		WSHandleConn[I](conn, h, ctx, config)
 	}
 }
 
-func WSHandleConn[I any, O any, S any](conn *websocket.Conn, h WSHandler[I, O, S], ctx S, config *WSConnConfig) {
+func WSHandleConn[I any, S WSConn[I]](conn *websocket.Conn, h WSHandler[I, S], ctx S, config *WSConnConfig) {
 	if config == nil {
 		config = DefaultWSConnConfig()
 	}
 	reader := conc.NewReader[I](func() (I, error) {
-		return h.ReadMessage(ctx, conn)
+		return ctx.ReadMessage(conn)
 	})
 	defer reader.Stop()
 
@@ -173,24 +108,24 @@ func WSHandleConn[I any, O any, S any](conn *websocket.Conn, h WSHandler[I, O, S
 	defer pingTimer.Stop()
 	defer pongChecker.Stop()
 
-	defer h.OnClose(ctx)
-	err := h.OnStart(ctx, conn)
+	defer ctx.OnClose()
+	err := ctx.OnStart(conn)
 	if err != nil {
 		return
 	}
 
 	conn.SetReadDeadline(time.Now().Add(config.PongPeriod))
-	h.SendPing(ctx)
+	ctx.SendPing()
 	for {
 		select {
 		case <-pingTimer.C:
-			h.SendPing(ctx)
+			ctx.SendPing()
 			break
 		case <-pongChecker.C:
 			hb_delta := time.Now().Sub(lastReadAt).Seconds()
 			if hb_delta > config.PongPeriod.Seconds() {
 				// Lost connection with conn so can drop off?
-				if h.OnTimeout(ctx) {
+				if ctx.OnTimeout() {
 					log.Printf("Last heart beat more than %d seconds ago.  Killing connection", int(hb_delta))
 					return
 				}
@@ -210,7 +145,7 @@ func WSHandleConn[I any, O any, S any](conn *websocket.Conn, h WSHandler[I, O, S
 							return
 						}
 					}
-					if h.OnError(ctx, result.Error) != nil {
+					if ctx.OnError(result.Error) != nil {
 						log.Println("Unknown Error: ", result.Error)
 						return
 					}
@@ -220,9 +155,80 @@ func WSHandleConn[I any, O any, S any](conn *websocket.Conn, h WSHandler[I, O, S
 				// dont need to do anything as we are using these for outbound connections
 				// only to write to a listening agent FE so can just log and drop any
 				// thing sent by agent FE here - this can change later
-				h.HandleMessage(ctx, result.Value)
+				ctx.HandleMessage(result.Value)
 			}
 			break
 		}
 	}
+}
+
+type JSONHandler struct {
+}
+
+type JSONConn struct {
+	Writer *conc.Writer[conc.Message[interface{}]]
+}
+
+func (j *JSONHandler) Validate(w http.ResponseWriter, r *http.Request) (out WSConn[interface{}], isValid bool) {
+	// All connections upgradeable
+	return &JSONConn{}, true
+}
+
+/**
+ * Reads the next message from the ws conn.
+ */
+func (j *JSONConn) ReadMessage(conn *websocket.Conn) (out interface{}, err error) {
+	err = conn.ReadJSON(&out)
+	return
+}
+
+/**
+ * Called to send the next ping message.
+ */
+func (j *JSONConn) SendPing() error {
+	j.Writer.Send(conc.Message[interface{}]{Value: gut.StringMap{"type": "ping"}})
+	return nil
+}
+
+/**
+ * Called to handle the next message from the input stream on the ws conn.
+ */
+func (j *JSONConn) HandleMessage(msg interface{}) error {
+	log.Println("Received Message: ", msg)
+	return nil
+}
+
+func (j *JSONConn) OnStart(conn *websocket.Conn) error {
+	j.Writer = conc.NewWriter(
+		func(msg conc.Message[interface{}]) error {
+			if msg.Error == io.EOF {
+				log.Println("Streamer closed...", msg.Error)
+				// do nothing
+				// SendJsonResponse(rw, nil, msg.Error)
+				return nil
+			} else if msg.Error != nil {
+				return WSConnWriteError(conn, msg.Error)
+			} else {
+				return WSConnWriteMessage(conn, msg.Value)
+			}
+		})
+	return nil
+}
+
+func (j *JSONConn) OnError(err error) error {
+	return err
+}
+
+/**
+ * Called when the connection closes.
+ */
+func (j *JSONConn) OnClose() {
+	// All the core hapens here
+	if j.Writer != nil {
+		j.Writer.Stop()
+	}
+}
+
+func (j *JSONConn) OnTimeout() bool {
+	return true
 }
