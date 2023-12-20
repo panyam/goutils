1889c70ad5df8497b03f686d25fbc7308f712666
Sri Panyam <sri.panyam@gmail.com>
2023-12-20 00:07:36 -0800

    Updated auth helpers

--- auth/router.go
+++ auth/router.go
@@ -2,16 +2,22 @@ package auth
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/panyam/goutils/utils"
-	"log"
-	"net/http"
 )
 
+type RequestHandler = func(ctx *gin.Context)
+
 type EnsureLoginConfig struct {
-	GetRedirURL   func(ctx *gin.Context) string
-	UserParamName string
+	CallbackURLParam   string
+	DefaultRedirectURL string
+	GetRedirURL        func(ctx *gin.Context) string
+	UserParamName      string
+	NoLoginRedirect    bool
 }
 
 /**
@@ -24,8 +30,17 @@ func EnsureLogin(config *EnsureLoginConfig) RequestHandler {
 	if config == nil {
 		config = &EnsureLoginConfig{}
 	}
-	if config.GetRedirURL == nil {
-		config.GetRedirURL = func(ctx *gin.Context) string { return "/auth/login" }
+	if config.UserParamName == "" {
+		config.UserParamName = "loggedInUserId"
+	}
+	if config.CallbackURLParam == "" {
+		config.CallbackURLParam = "/callbackURL"
+	}
+	if config.DefaultRedirectURL == "" {
+		config.DefaultRedirectURL = "/login"
+	}
+	if config.GetRedirURL == nil && !config.NoLoginRedirect {
+		config.GetRedirURL = func(ctx *gin.Context) string { return config.DefaultRedirectURL }
 	}
 	if config.UserParamName == "" {
 		config.UserParamName = "loggedInUser"
@@ -37,17 +52,23 @@ func EnsureLogin(config *EnsureLoginConfig) RequestHandler {
 			log.Println("No user Found: ", config.UserParamName, session)
 			// Redirect to a login if user not logged in
 			// `/${config.redirectURLPrefix || "auth"}/login?callbackURL=${encodeURIComponent(req.originalUrl)}`;
-			redirUrl := config.GetRedirURL(ctx)
+			redirUrl := ""
+			if config.GetRedirURL != nil {
+				redirUrl = config.GetRedirURL(ctx)
+			}
 			if redirUrl != "" {
 				originalUrl := ctx.Request.URL.Path
 				encodedUrl := utils.EncodeURIComponent(originalUrl)
-				fullRedirUrl := fmt.Sprintf("%s?callbackURL=%s", redirUrl, encodedUrl)
+				fullRedirUrl := fmt.Sprintf("%s?%s=%s", redirUrl, config.CallbackURLParam, encodedUrl)
 				ctx.Redirect(http.StatusFound, fullRedirUrl)
 			} else {
 				// otherwise a 401
 				ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Not LoggedIn"})
 			}
 			ctx.Abort()
+		} else {
+			log.Println("Setting Logged In User Id: ", userParam)
+			ctx.Set("loggedInUserId", userParam)
 		}
 	}
 }
