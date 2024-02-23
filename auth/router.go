7a4cb9ec1757c9530c7beef31ead512f180255fd
Sri Panyam <sri.panyam@gmail.com>
2024-02-22 23:47:18 -0800

    simplifying auth configs and request vars

--- auth/router.go
+++ auth/router.go
@@ -5,18 +5,16 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-gonic/gin"
+	gohttp "github.com/panyam/goutils/http"
 	"github.com/panyam/goutils/utils"
 )
 
-type RequestHandler = func(ctx *gin.Context)
-
-type EnsureLoginConfig struct {
+type AuthConfig struct {
+	RequestVars        *gohttp.RequestVarMap
+	SessionGetter      func(r *http.Request, w http.ResponseWriter, param string) any
 	CallbackURLParam   string
 	DefaultRedirectURL string
-	GetRedirURL        func(ctx *gin.Context) string
-	SessionGetter      func(ctx *gin.Context, param string) any
+	GetRedirURL        func(r *http.Request) string
 	UserParamName      string
 	NoLoginRedirect    bool
 }
@@ -27,53 +25,42 @@ type EnsureLoginConfig struct {
  * @param res Response object
  * @param next next function
  */
-func EnsureLogin(config *EnsureLoginConfig) RequestHandler {
-	if config == nil {
-		config = &EnsureLoginConfig{}
+func (a *AuthConfig) EnsureLogin(w http.ResponseWriter, r *http.Request) {
+	if a.UserParamName == "" {
+		a.UserParamName = "loggedInUserId"
 	}
-	if config.UserParamName == "" {
-		config.UserParamName = "loggedInUserId"
+	if a.CallbackURLParam == "" {
+		a.CallbackURLParam = "/callbackURL"
 	}
-	if config.CallbackURLParam == "" {
-		config.CallbackURLParam = "/callbackURL"
+	if a.DefaultRedirectURL == "" {
+		a.DefaultRedirectURL = "/login"
 	}
-	if config.DefaultRedirectURL == "" {
-		config.DefaultRedirectURL = "/login"
+	if a.GetRedirURL == nil && !a.NoLoginRedirect {
+		a.GetRedirURL = func(r *http.Request) string { return a.DefaultRedirectURL }
 	}
-	if config.GetRedirURL == nil && !config.NoLoginRedirect {
-		config.GetRedirURL = func(ctx *gin.Context) string { return config.DefaultRedirectURL }
+	if a.UserParamName == "" {
+		a.UserParamName = "loggedInUser"
 	}
-	if config.UserParamName == "" {
-		config.UserParamName = "loggedInUser"
-	}
-	if config.SessionGetter == nil {
-		config.SessionGetter = func(ctx *gin.Context, param string) any {
-			session := sessions.Default(ctx)
-			return session.Get(param)
+	userParam := a.SessionGetter(r, w, a.UserParamName)
+	if userParam == "" || userParam == nil {
+		// Redirect to a login if user not logged in
+		// `/${a.redirectURLPrefix || "auth"}/login?callbackURL=${encodeURIComponent(req.originalUrl)}`;
+		redirUrl := ""
+		if a.GetRedirURL != nil {
+			redirUrl = a.GetRedirURL(r)
 		}
-	}
-	return func(ctx *gin.Context) {
-		userParam := config.SessionGetter(ctx, config.UserParamName)
-		if userParam == "" || userParam == nil {
-			// Redirect to a login if user not logged in
-			// `/${config.redirectURLPrefix || "auth"}/login?callbackURL=${encodeURIComponent(req.originalUrl)}`;
-			redirUrl := ""
-			if config.GetRedirURL != nil {
-				redirUrl = config.GetRedirURL(ctx)
-			}
-			if redirUrl != "" {
-				originalUrl := ctx.Request.URL.Path
-				encodedUrl := utils.EncodeURIComponent(originalUrl)
-				fullRedirUrl := fmt.Sprintf("%s?%s=%s", redirUrl, config.CallbackURLParam, encodedUrl)
-				ctx.Redirect(http.StatusFound, fullRedirUrl)
-			} else {
-				// otherwise a 401
-				ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Not LoggedIn"})
-			}
-			ctx.Abort()
+		if redirUrl != "" {
+			originalUrl := r.URL.Path
+			encodedUrl := utils.EncodeURIComponent(originalUrl)
+			fullRedirUrl := fmt.Sprintf("%s?%s=%s", redirUrl, a.CallbackURLParam, encodedUrl)
+			http.Redirect(w, r, fullRedirUrl, http.StatusFound)
 		} else {
-			log.Println("Setting Logged In User Id: ", userParam)
-			ctx.Set("loggedInUserId", userParam)
+			// otherwise a 401
+			http.Error(w, "Failed", http.StatusUnauthorized)
 		}
+	} else if a.RequestVars != nil {
+		log.Println("Setting Logged In User Id: ", userParam)
+		// ctx.Set("loggedInUserId", userParam)
+		a.RequestVars.SetKey(r, "loggedInUser", userParam)
 	}
 }
--- fn/functools.go
+++ fn/functools.go
@@ -1,4 +1,4 @@
-package utils
+package fn
 
 import (
 	"sync"
