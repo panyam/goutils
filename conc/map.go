1d38ef40f55ee4b3fba2288e92e820937a1476be
Sri Panyam <sri.panyam@gmail.com>
2024-03-26 12:39:12 -0700

    reversing L and non L version with latter defaulting to getting a lock

--- conc/map.go
+++ conc/map.go
@@ -30,7 +30,7 @@ func (m *Map[K, V]) Unlock() {
 	m.rwmutex.Unlock()
 }
 
-func (m *Map[K, V]) Has(k K, lock bool) bool {
+func (m *Map[K, V]) LHas(k K, lock bool) bool {
 	if lock {
 		m.rwmutex.RLock()
 		defer m.rwmutex.RUnlock()
@@ -39,7 +39,7 @@ func (m *Map[K, V]) Has(k K, lock bool) bool {
 	return exists
 }
 
-func (m *Map[K, V]) Get(k K, lock bool) (V, bool) {
+func (m *Map[K, V]) LGet(k K, lock bool) (V, bool) {
 	if lock {
 		m.rwmutex.RLock()
 		defer m.rwmutex.RUnlock()
@@ -48,7 +48,7 @@ func (m *Map[K, V]) Get(k K, lock bool) (V, bool) {
 	return out, ok
 }
 
-func (m *Map[K, V]) Set(k K, v V, lock bool) {
+func (m *Map[K, V]) LSet(k K, v V, lock bool) {
 	if lock {
 		m.rwmutex.Lock()
 		defer m.rwmutex.Unlock()
@@ -56,7 +56,7 @@ func (m *Map[K, V]) Set(k K, v V, lock bool) {
 	m.store[k] = v
 }
 
-func (m *Map[K, V]) Delete(k K, lock bool) {
+func (m *Map[K, V]) LDelete(k K, lock bool) {
 	if lock {
 		m.rwmutex.Lock()
 		defer m.rwmutex.Unlock()
@@ -64,7 +64,7 @@ func (m *Map[K, V]) Delete(k K, lock bool) {
 	delete(m.store, k)
 }
 
-func (m *Map[K, V]) Range(lock bool, meth func(K, V) bool) {
+func (m *Map[K, V]) LRange(lock bool, meth func(K, V) bool) {
 	if lock {
 		m.rwmutex.RLock()
 		defer m.rwmutex.RUnlock()
@@ -82,28 +82,28 @@ func (m *Map[K, V]) View(actions func()) {
 	m.rwmutex.RUnlock()
 }
 
-func (m *Map[K, V]) Update(actions func()) {
+func (m *Map[K, V]) Update(actions func(items map[K]V)) {
 	m.rwmutex.Lock()
-	actions()
+	actions(m.store)
 	m.rwmutex.Unlock()
 }
 
-func (m *Map[K, V]) LHas(k K) bool {
-	return m.Has(k, true)
+func (m *Map[K, V]) Has(k K) bool {
+	return m.LHas(k, true)
 }
 
-func (m *Map[K, V]) LGet(k K) (V, bool) {
-	return m.Get(k, true)
+func (m *Map[K, V]) Get(k K) (V, bool) {
+	return m.LGet(k, true)
 }
 
-func (m *Map[K, V]) LSet(k K, v V) {
-	m.Set(k, v, true)
+func (m *Map[K, V]) Set(k K, v V) {
+	m.LSet(k, v, true)
 }
 
-func (m *Map[K, V]) LDelete(k K) {
-	m.Delete(k, true)
+func (m *Map[K, V]) Delete(k K) {
+	m.LDelete(k, true)
 }
 
-func (m *Map[K, V]) LRange(meth func(K, V) bool) {
-	m.Range(true, meth)
+func (m *Map[K, V]) Range(meth func(K, V) bool) {
+	m.LRange(true, meth)
 }
